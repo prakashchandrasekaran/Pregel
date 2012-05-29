20e6217bb5be00732a1b4ef49e7f6db6bbce8ab6
Gautham Narayanasamy <gautham.n.sam@gmail.com>
2012-05-28 19:22:39 -0700

    Added logic to reload the properties file whenever it is modified. Renamed props file to system.properties.

--- Pregel/src/main/java/utility/Props.java
+++ Pregel/src/main/java/utility/Props.java
@@ -1,42 +1,77 @@
 package utility;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Props {
 
 	private static Props props;
+	
 	private static Properties properties;
 	
-	private Props(String propertyFile) {
-		try {
-			properties = new Properties();
-			properties.load(new FileInputStream(propertyFile));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static File propertiesFile;
+	
+	private static long lastModified;
+	
+	private Props(String propertiesFilePath) {
+			properties = new Properties();			
+			propertiesFile = new File(propertiesFilePath);
+			loadProperties();
 	}
 	
 	public static synchronized Props getInstance() {
 		if(props == null) {
-			props = new Props("props");		
+			props = new Props("../../config/system.properties");		
 		}
 		return props;
 	}
 	
+	/**
+	 * Checks if the properties file has been modified after it was loaded.
+	 */
+	private static void checkProperties(){
+		if(propertiesFile.lastModified() > lastModified){
+			loadProperties();
+		}
+	}
+	
+	private static void loadProperties(){
+		lastModified = System.currentTimeMillis();
+		InputStream in = null;
+		try{
+			in = new FileInputStream(propertiesFile);
+			properties.load(in);
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	public String getStringProperty(String key) {
+		checkProperties();
 		return properties.getProperty(key);
 	}
 	
 	public Integer getIntProperty(String key) {
+		checkProperties();
 		return Integer.parseInt(properties.getProperty(key));
 	}
 	
 	public Double getDoubleProperty(String key) {
+		checkProperties();
 		return Double.parseDouble(properties.getProperty(key));
 	}
 	
