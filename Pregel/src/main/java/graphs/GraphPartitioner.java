b480edc8776396b081655a54f2d8d3d7b4a6a093
Vijayaraghavan Subbaiah <connectingvijay@gmail.com>
2012-05-28 19:41:02 -0700

    fixed

--- Pregel/src/main/java/graphs/GraphPartitioner.java
+++ Pregel/src/main/java/graphs/GraphPartitioner.java
@@ -6,7 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
+import utility.VertexGenerator;
 import api.Partition;
 import api.Vertex;
 
