cb56a4a084e33459a8a7dbe78fb9d1418808e776
PrakashChandrasekaran <techianprakash@gmail.com>
2012-05-28 19:49:27 -0700

    Constants are read from Property file instead of hard-coded values
    changes are made in VertexGenerator.java file ..

--- Pregel/src/main/java/utility/VertexGenerator.java
+++ Pregel/src/main/java/utility/VertexGenerator.java
@@ -18,13 +18,17 @@ public class VertexGenerator {
 	 private static String sourceVertexDelimiter = "-";
 	 private static String edgesDelimiter = ",";
 	 private static String vertexWeightDelimiter = ":";
-	
+	 private static Props props;
 	 private VertexGenerator() { }
 	
 	 public static synchronized VertexGenerator getInstance() {
 		  if (instance == null) {
-		   instance = new VertexGenerator();
+			  instance = new VertexGenerator();
 		  }
+		  props = Props.getInstance();
+		  sourceVertexDelimiter = props.getStringProperty("VERTEX_LIST_SEPARATOR");
+		  edgesDelimiter = props.getStringProperty("LIST_VERTEX_SEPARATOR");
+		  vertexWeightDelimiter = props.getStringProperty("LIST_VERTEX_WEIGHT_SEPARATOR");
 		  return instance;
 	 }
 	 
