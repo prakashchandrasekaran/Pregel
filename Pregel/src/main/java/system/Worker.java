680325348ee6d3f3efd399ec3efb90e5242ecb5d
Vijayaraghavan Subbaiah <connectingvijay@gmail.com>
2012-05-30 03:00:04 -0700

    Added Worker stubs

---
+++ Pregel/src/main/java/system/Worker.java
@@ -0,0 +1,17 @@
+package system;
+
+import java.util.List;
+import api.Partition;
+
+public class Worker {
+	/**
+	 * Adds the list of partitions to be assigned to the worker
+	 * 
+	 * @param partitionList
+	 *            Represents a list of partition to be assigned to the worker
+	 */
+	public void addPartitions(List<Partition> partitionList) {
+
+	}
+
+}
