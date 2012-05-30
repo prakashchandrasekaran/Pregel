10788895068ba669a8920b7ec6dbaff62a1141db
PrakashChandrasekaran <techianprakash@gmail.com>
2012-05-30 13:53:04 -0700

    new Exception added : PropertyNotFoundException

---
+++ Pregel/src/main/java/exceptions/PropertyNotFoundException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class PropertyNotFoundException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public PropertyNotFoundException(String propertyKey) {
+		super(propertyKey + " is not found in props file");
+	}
+}
