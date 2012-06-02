08b6957a33c8501892f37eefc30ff67f63c3c8f4
PrakashChandrasekaran <techianprakash@gmail.com>
2012-06-02 01:16:31 -0700

    added Message class

---
+++ Pregel/src/main/java/system/Message.java
@@ -0,0 +1,19 @@
+package system;
+
+import graphs.VertexID;
+
+public class Message {
+
+	private VertexID sourceID;
+	private VertexID destID;
+	private Data data;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
