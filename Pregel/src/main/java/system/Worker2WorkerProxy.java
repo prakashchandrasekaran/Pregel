33619f6118d4c3af8f2432aa2ad312f5e7f7f454
PrakashChandrasekaran <techianprakash@gmail.com>
2012-06-02 00:35:10 -0700

    W2WProxy added

---
+++ Pregel/src/main/java/system/Worker2WorkerProxy.java
@@ -0,0 +1,14 @@
+package system;
+
+public class Worker2WorkerProxy {
+	private Worker receiverWorker;
+	
+	public Worker2WorkerProxy(Worker worker) {
+		this.receiverWorker = worker;
+	}
+	
+	public void send() {
+		
+	}
+
+}
