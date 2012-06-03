fa7dd453f23cd9c9286c4f00715dbcfd2e307424
Vijayaraghavan Subbaiah <connectingvijay@gmail.com>
2012-06-03 13:33:42 -0700

    Added PageRankVertex and PageRankData

---
+++ Pregel/src/main/java/client/PageRankData.java
@@ -0,0 +1,55 @@
+/*
+ * @author sam
+ */
+package client;
+
+import api.Data;
+
+/**
+ * Defines the implementation of Data that is specific to the Shortest-Path graph problem.
+ * 
+ * @author Prakash Chandrasekaran
+ * @author Gautham Narayanasamy
+ * @author Vijayaraghavan Subbaiah
+ */
+public class PageRankData implements Data<Double>{
+	
+	Double value;
+	
+	private static final long serialVersionUID = -3455938165928334533L;
+	
+	public PageRankData(Double value){
+		this.value = value;
+	}
+	
+	/* (non-Javadoc)
+	 * @see api.Data#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(Data<Double> other) {
+		if(this.getValue() == other.getValue()){
+			return 0;
+		}
+		else if(this.getValue() > other.getValue()){
+			return 1;
+		}
+		return -1;
+	}
+
+	/* (non-Javadoc)
+	 * @see api.Data#getValue()
+	 */
+	@Override
+	public Double getValue() {
+		return value;
+	}
+
+	/* (non-Javadoc)
+	 * @see api.Data#setValue(java.lang.Object)
+	 */
+	@Override
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+}
